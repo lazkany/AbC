3f456e343f80893c93fda95f868bb691e108eeb9
lazkany <yehia.abdalrahman@imtlucca.it>
2016-03-11 09:11:26 +0000

    changes

---
+++ org.sysma.abc.core/src/org/sysma/abc/core/InputAction.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.sysma.abc.core;
+
+/**
+ * @author Yehia Abd Alrahman
+ *
+ */
+public class InputAction {
+
+	/**
+	 * 
+	 */
+	public InputAction() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ org.sysma.abc.core/src/org/sysma/abc/core/OuputAction.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.sysma.abc.core;
+
+/**
+ * @author Yehia Abd Alrahman
+ *
+ */
+public class OuputAction {
+
+	/**
+	 * 
+	 */
+	public OuputAction() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
